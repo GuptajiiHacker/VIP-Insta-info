583a02337201db0e2ded2781298b5bcbb1536ef7
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-01-17 21:35:02 +0530

    Update instaOSINT.py

--- instaOSINT.py
+++ instaOSINT.py
@@ -33,8 +33,9 @@ try:
         {Gr}██║██║ ╚████║███████║   ██║   ██║  ██║      {Re}╚██████╔╝███████║██║██║ ╚████║   ██║   
         {Gr}╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝       {Re}╚═════╝ ╚══════╝╚═╝╚═╝  ╚═══╝   ╚═╝   
         {Wh}  <----- {Gr}O S I N T       {Wh}I N S T A G R A M       {Wh}B Y      {Gr}H U N B Y T S {Wh}----->  
-    print(f"\n                              {Wh}[ {Gr}! {Wh}] 
+                          {Wh}[ {Gr}! {Wh}] 
     LOGIN YOUR INSTAGRAM ACCOUNT")
+    print(f"\n
     US = input(f"\n       [ {Gr}+ {Wh}] INPUT USERNAME INSTAGRAM ACCOUNT : {Re}")
     PW = input(f"       {Wh}[ {Gr}+ {Wh}] INPUT PASSWORD INSTAGRAM ACCOUNT : {Re}")
 
