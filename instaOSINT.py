7a406fd37157b24559acabf1db66b6a0b39957a7
GuptajiiHacker <155041576+GuptajiiHacker@users.noreply.github.com>
2024-01-17 21:39:08 +0530

    instaOSINT.py

--- instaOSINT.py
+++ instaOSINT.py
@@ -35,8 +35,8 @@ try:
         {Wh}  <----- {Gr}O S I N T       {Wh}I N S T A G R A M       {Wh}B Y      {Gr}H U N B Y T S {Wh}----->  
                           {Wh}[ {Gr}! {Wh}] 
                   LOGIN YOUR INSTAGRAM ACCOUNT
-    print(f"\n
-    US = input(f"\n       [ {Gr}+ {Wh}] INPUT USERNAME INSTAGRAM ACCOUNT : {Re}")
+    print(f"
+    US = input(f"        [ {Gr}+ {Wh}] INPUT USERNAME INSTAGRAM ACCOUNT : {Re}")
     PW = input(f"       {Wh}[ {Gr}+ {Wh}] INPUT PASSWORD INSTAGRAM ACCOUNT : {Re}")
 
     os.system('clear')
